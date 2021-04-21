b53b7b4f778d33cf3a446ade52d3e71acfd25f07
N.K <mrsk1157@hotmail.co.jp>
2021-04-21 14:22:50 +0900

    Sample8_3追加

---
+++ lesson8/Sample8_3.java
@@ -0,0 +1,29 @@
+package lesson8;
+//車クラス
+class Car3{
+  int num;
+  double gas;
+
+  void show(){
+    System.out.println("車のナンバーは" + num + "です。");
+    System.out.println("ガソリン量は" + gas + "です。");
+  }
+  void showCar3(){
+    System.out.println("これから車の情報を表示します。");
+    //this.showも可
+    this.show();
+  }
+}
+
+public class Sample8_3 {
+  public static void main(String[] args){
+    Car3 car1;
+
+    car1 = new Car3();
+
+    car1.num = 1234;
+    car1.gas = 20.5;
+
+    car1.showCar3();
+  }
+}
