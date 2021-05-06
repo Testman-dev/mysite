524fd882b983e73a3d17a2a86ac0a47c1c9d597e
N.K <mrsk1157@hotmail.co.jp>
2021-05-06 14:49:03 +0900

    Sample13_6追加、微調整

--- lesson13/Pb/Sample13_5.java
+++ lesson13/Pb/Sample13_5.java
@@ -1,5 +1,7 @@
 package lesson13.Pb;
 
+//異なるパッケージのクラスを利用する
+
 public class Sample13_5 {
   public static void main(String[] args){
     lesson13.Pc.Car car1 = new lesson13.Pc.Car();
---
+++ lesson13/Pb/Sample13_6.java
@@ -0,0 +1,12 @@
+package lesson13.Pb;
+
+//import文を使う
+
+import lesson13.Pc.Car;
+
+public class Sample13_6 {
+  public static void main(String[] args){
+    Car car1 = new Car();
+    car1.show();
+  }
+}
