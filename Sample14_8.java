8299393834be17ad1e634c1eafc1d418db2a0614
N.K <mrsk1157@hotmail.co.jp>
2021-05-07 14:44:12 +0900

    Sample14_8追加

---
+++ Sample14_8.java
@@ -0,0 +1,24 @@
+//ファイルから入力する
+
+import java.io.*;
+
+public class Sample14_8 {
+  public static void main(String[] args){
+    try{
+      BufferedReader br = new BufferedReader(new FileReader("test1.txt"));
+
+      //1行ずつ読み込む
+      String str1 = br.readLine();
+      String str2 = br.readLine();
+
+      System.out.println("ファイルに書き込まれている2つの文字列は");
+      System.out.println(str1 + "です。");
+      System.out.println(str2 + "です。");
+
+      br.close();
+    }
+    catch(IOException e){
+      System.out.println("入出力エラーです。");
+    }
+  }
+}
