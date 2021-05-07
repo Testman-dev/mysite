98dadd60828521836c6a490319da6856859cac4b
N.K <mrsk1157@hotmail.co.jp>
2021-05-07 14:50:36 +0900

    Sample14_8配置調整

--- lesson14/Sample14_8.java
+++ lesson14/Sample14_8.java
@@ -1,3 +1,5 @@
+package lesson14;
+
 //ファイルから入力する
 
 import java.io.*;
@@ -5,6 +7,7 @@ import java.io.*;
 public class Sample14_8 {
   public static void main(String[] args){
     try{
+      //test1.txtを読み込むことを宣言する
       BufferedReader br = new BufferedReader(new FileReader("test1.txt"));
 
       //1行ずつ読み込む
